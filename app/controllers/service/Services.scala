118989d4f815604d9d84bb726600255a169d2a71
sangsang-hu <sangsang-hu@rontech.co.jp>
2014-04-01 16:50:59 +0800

    Change the view of updataService to editSalonService.

--- app/controllers/service/Services.scala
+++ app/controllers/service/Services.scala
@@ -87,7 +87,7 @@ object Services extends Controller {
  */  
   def updateService(id: ObjectId, salonId: ObjectId) = Action { implicit request =>
     serviceUpdateForm().bindFromRequest.fold(
-      errors => BadRequest(views.html.service.updateService(Salon.findById(salonId).get,errors,Service.findOneById(id).get)),
+      errors => BadRequest(views.html.salon.admin.editSalonService(Salon.findById(salonId).get,errors,Service.findOneById(id).get)),
       {
         service =>
           Service.save(service.copy(id = id), WriteConcern.Safe)
