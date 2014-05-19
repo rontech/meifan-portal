d7e009c1dcd0c7b3b8b17bcd2bc52087e50824f5
zhenglu316 <lu-zheng@sz-rontech.com>
2014-05-19 18:09:04 +0800

    Change the format of the code.

--- public/javascripts/httpError.js
+++ public/javascripts/httpError.js
@@ -1,22 +1,22 @@
-function followedCouponIsForbidden(userId){
-	jsRoutes.controllers.auth.MyFollows.followedCoupon(userId).ajax(ajax2);
+function followedCouponIsForbidden(userId) {
+  jsRoutes.controllers.auth.MyFollows.followedCoupon(userId).ajax(ajax2);
 }
 
-var successFn = function(data) {
+var successFn = function (data) {
 };
 
-var errorFn = function(err) {
-	if(err.readyState == 4 && err.status == 403){
-		alert("您和该用户未互相关注，无法查看该内容。");
-	}
-	
-	if(err.readyState ==4 && err.status ==401){
-		alert("无法查看该内容，请您先登录。")
-	}
- }
-	   
+var errorFn = function (err) {
+  if (err.readyState == 4 && err.status == 403) {
+    alert("您和该用户未互相关注，无法查看该内容。");
+  }
+
+  if (err.readyState == 4 && err.status == 401) {
+    alert("无法查看该内容，请您先登录。")
+  }
+}
+
 ajax2 = {
-      async:false,
-	  success: successFn,
-	  error: errorFn
-	 }
+  async: false,
+  success: successFn,
+  error: errorFn
+}
